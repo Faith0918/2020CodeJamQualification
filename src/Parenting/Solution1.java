b7330f2ff687e4fb9453301f7363ddf3b68faee8
Faith0918 <sinsil0918@gmail.com>
2020-04-08 17:51:23 +0900

    fixed

--- src/Parenting/Solution1.java
+++ src/Parenting/Solution1.java
@@ -25,23 +25,27 @@ public class Solution1 {
 				S[i][4] = 0;//0 1 as C, 2 as J, 0 as non
 				
 			}
+			
 			bfs(S, N, 0);
 			for(int i = 0; i<N; i++) {
-				if(S[i][4] == 0||(S[i][2] == 0 && S[i][3] == 0)) {
+				if((S[i][2] == 0 && S[i][3] == 0)) {
 					impossible = true;
 					answer = "IMPOSSIBLE";
 					break;
 				}
 				else {
-					if(S[i][4] == 1) {
+					if(S[i][2] == 0 && S[i][3] == 1) {
 						System.out.println("S[i][4] : "+S[i][4]);
 						System.out.println(S[i][2]+" "+S[i][3]);
 						answer =  answer + 'C';
 					}
-					else {
+					else if(S[i][2] == 1 && S[i][3] == 0) {
 						System.out.println("S[i][4] : "+S[i][4]);
 						System.out.println(S[i][2]+" "+S[i][3]);
 						answer= answer+'J';
+					}
+					else {
+						System.out.println("else");
 					}
 				}
 			}
@@ -49,51 +53,48 @@ public class Solution1 {
 			
 			
 		}
+		sc.close();
 	}
 	private static void bfs(int[][] S, int N, int count) {
 		if(count == N) {
 			return;
 		}
-		for(int i = 0; i<N; i++) {
-			if(S[i][2] == 0 && S[i][3] == 0) {
-				return;
+		else {
+			System.out.println("bfs");
+			for(int i = 0; i<N; i++) {
+				if(S[i][2] == 0 && S[i][3] == 0) {
+					System.out.println("1");
+					return;
+				}
+				else if(S[i][2] == 0) {
+					System.out.println("2");
+					CheckOverlap(S, N, i);
+				}
+				else if(S[i][3] == 0) {
+					System.out.println("3");
+					CheckOverlap(S, N, i);
+				}
+				else {
+					System.out.println("4");
+					S[i][2] = 0;
+					CheckOverlap(S, N, i);
+				}
 			}
-		}
-		
-		for(int i = count; i<N; i++) {
-			
-			if(S[i][2]==1 && S[i][3] == 1) {
-				S[i][2] = 0;//C allocate
-				S[i][4] = 1;
-				
-				int[][] newS = CheckOverlap(S, N, count);
-				bfs(newS, N, count+1);
-				S[i][2] = 1;//C allocate
-				
-			}
-			else if(S[i][2] == 1) {
-				S[i][4] = 1;
-				int[][] newS = CheckOverlap(S,  N, count);
-				bfs(newS, N, count+1);
-			}
-			else {
-				S[i][4] = 2;
-				int[][] newS = CheckOverlap(S, N, count);
-				bfs(newS, N, count+1);
-//				S[i][4] = 0;
-			}
+			bfs(S, N, count+1);
 		}
 		
 	}
-	private static int[][] CheckOverlap(int[][] S, int N, int count) {
+	private static void CheckOverlap(int[][] S, int N, int count) {
+		int[][] newS = S;
 		for(int i = 0; i<N; i++ ) {
-			if(i != count||!(S[i][2] == 0 &&S[i][4] ==1)||!(S[i][3] ==0 && S[i][4] == 2)) {
-				if( (S[i][1]>S[count][0] && S[i][1]<=S[count][1])||(S[i][0]>=S[count][0] && S[i][0]<S[count][1])) {
-					if(S[count][4] == 1) {
+			if(i != count) {
+				if( (newS[i][1]>newS[count][0] && newS[i][1]<=newS[count][1])||(newS[i][0]>=newS[count][0] && S[i][0]<S[count][1])) {
+					if(S[count][2] ==  0 && S[count][3] == 1) {
 						S[i][2] = 0;
 						CheckOverlap(S, N, i);
+						System.out.println("checkoverlap");
 					}
-					else if(S[count][4] == 2) {
+					else if(S[count][2] == 1 && S[count][3] == 0) {
 						S[i][3] = 0;
 						CheckOverlap(S, N, i);
 					}
@@ -101,8 +102,7 @@ public class Solution1 {
 			}
 			
 		}
-		return S;
-		
+
 		
 	}
 }
